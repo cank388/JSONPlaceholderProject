75317f0ed28c5c46ed44e6a932d6473bbe548d6f
Can Kalender <cankalender.tr@gmail.com>
2025-01-18 21:46:20 +0300

    Simple user list vc created.

---
+++ JSONPlaceholderProject/Scenes/UserList/UserListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserListViewController.swift
+//  JSONPlaceholderProject
+//
+//  Created by Can Kalender on 18.01.2025.
+//
+
+import UIKit
+
+final class UserListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ JSONPlaceholderProject/Scenes/UserList/UserListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserListViewModel.swift
+//  JSONPlaceholderProject
+//
+//  Created by Can Kalender on 18.01.2025.
+//
+
+import Foundation
+
+final class UserListViewModel {
+    
+}
